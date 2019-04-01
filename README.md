adf2e7bbb37bbeddef6edb70802f5e7bf72b18dc
houking-can <1240723224@qq.com>
2019-04-01 11:49:19 +0800

    push project

--- README.md
+++ README.md
@@ -9,19 +9,19 @@ Notice: To run this project, you must install [Adobe Acrobat DC](https://www.ado
     * Acrobat   
     * Adobe Acrobat 10.0 Type Library  
     * System Windows Forms  
-They are two COM type libraries, after installing Adobe Acrobat DC, you can add reference in Visual Studio with project manager.
+- The fist two are COM type libraries, after installing Adobe Acrobat DC, you can add reference in Visual Studio with project manager.
 
 ## How to build and run it
 1. Create a C# Console project, and choose the .Net framework version. 
 2. Add references, click the COM in references manager and select Acrobat and Adobe Acrobat 10.0 Type Library.
-3. To run this project, you need add command parameters in project manager, the input file  complete path and output dictionary(optional, if not specify, it will save the output file where the executable file in). You can also use console to run the executable file.  
-Like this: pdf2html.exe inputfile outputdir
+3. To run this project, you need add command parameters in project manager, the input file  complete path and output dictionary(optional, if not specify, it will save the output file where the executable file in). You can also use console to run the executable file as follows:
+    * pdf2html.exe inputfile outputdir
 4. If you run this repository directly, you may skip step 1 and 2. Just compile and run in Visual Studio.
 
 ## Extension
-If you want to convert pdf to other formats, such as .doc , .txt, etc. Just modify a line code in:  
-object[] saveAsParam1 = { savefileName + ".html", "com.adobe.acrobat.html-3-20" };  
-"com.adobe.acrobat.html-3-20" is the cConID support by Acrobat library. The list string ids are below:  
+If you want to convert pdf to other formats, such as doc , txt, etc. Just modify a line code in:  
+<center>object[] saveAsParam1 = { savefileName + ".html", "com.adobe.acrobat.html-3-20" };</center>  
+cConvIDs are supported by Acrobat library. The string ids are as follows:  
 
 cConvID| extension | comment  
 -|:-: |:-:
@@ -52,6 +52,8 @@ com.adobe.acrobat.htm l- 4-01-css-1-00 | com.adobe.acrobat.html
 Refer to [Acrobat SDK and documents](https://www.adobe.com/devnet/acrobat/documentation.html) to learn more. You can download the SDK package, and develop application on the samples.
 
 # Statement
-The source code is for learning and communication only. Please contact [Adobe](https://www.adobe.com/) for commercial use.
+The source code is for learning and communication only.   
+Please contact [Adobe](https://www.adobe.com/cn/) for commercial use.
+
 
 
---
+++ pdf2html/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.IO;
+using Acrobat;
+using System.Reflection;
+using System.Windows.Forms;
+
+public class JSObject
+{
+    private AcroAVDoc g_AVDoc = null;
+
+    public int Convert_pdf_to_html(String fileName, String saveDir)
+    {
+
+        FileInfo fileinfo = new FileInfo(fileName);
+        //file exits
+        if (fileinfo.Exists){
+            if (g_AVDoc != null){
+                g_AVDoc.Close(0);
+            }
+            g_AVDoc = new AcroAVDoc();
+            g_AVDoc.Open(fileName, "");
+        }//file not exist
+        else{
+            Console.WriteLine("{0} not exist!", fileName);
+            return 2;
+        }
+        // if open pdf success
+        if (g_AVDoc.IsValid()){
+            String savefileName = "";
+            if (Directory.Exists(saveDir)){
+                savefileName = saveDir + '\\' + Path.GetFileNameWithoutExtension(fileName);
+            }
+            else{
+                savefileName = Path.GetDirectoryName(saveDir) + Path.GetFileNameWithoutExtension(saveDir);
+            }
+
+            CAcroPDDoc pdDoc = (CAcroPDDoc)g_AVDoc.GetPDDoc();
+            //Acquire the Acrobat JavaScript Object interface from the PDDoc object
+            Object jsObj = pdDoc.GetJSObject();
+            Type T = jsObj.GetType();
+
+            object[] saveAsParam1 = { savefileName + ".html", "com.adobe.acrobat.html-3-20" };
+            //object[] saveAsParam2 = { savefileName + ".xml", "com.adobe.acrobat.xml-1-00" };
+            //object[] saveAsParam3 = { savefileName + ".docx", "com.adobe.acrobat.docx" };
+            T.InvokeMember(
+                            "saveAs",
+                            BindingFlags.InvokeMethod |
+                            BindingFlags.Public |
+                            BindingFlags.Instance,
+                            null, jsObj, saveAsParam1);
+
+            // close object
+            g_AVDoc.Close(0);
+            Console.WriteLine("Convert PDF to HTML success.");
+            return 0;
+        }//Open file error
+        else{
+            Console.WriteLine("Open {0} failed!", fileName);
+            return 3;
+        }
+    }
+}
+
+
+static class Program
+{
+    static int Main(string[] args)
+    {
+        String fileName;
+        // set the default output dictionary
+        String saveDir = Application.StartupPath;
+        // check the args
+        if (args.Length == 1){
+            fileName = args[0];
+        }
+        else if (args.Length == 2){
+            fileName = args[0];
+            saveDir = args[1];
+        }
+        else{
+            Console.WriteLine("Usage: PDF2XML.exe inputfile outputdir(optional).");
+            return 1;
+        }
+
+        /*
+         retNumber:
+            0 convert sucess
+            1 parameters error
+            2 inpout file not exist
+            3 open pdf error
+         */
+
+        int retNumber;
+        JSObject AdobeJS = new JSObject();
+        retNumber = AdobeJS.Convert_pdf_to_html(fileName, saveDir);
+        return retNumber;
+    }
+}
